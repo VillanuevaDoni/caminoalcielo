72a49b1b0ec34a3d6e4da4d1b0bb649aa4476ccb
VillanuevaDoni <donovanreu88@outlook.com>
2024-03-07 05:59:34 -0500

    Se cambio la posicion de la imagen en la pagina quienes somos

--- src/views/QuienesSomos.jsx
+++ src/views/QuienesSomos.jsx
@@ -12,16 +12,6 @@ useEffect(()=>{
     <div className="container-fluid p-4">
       <div data-aos="fade-up" className="container col-xxl-8 px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5 rounded-3 ">
-          <div data-aos="fade-up" className="col-10 col-sm-8 col-lg-6">
-            <img 
-              src={ImgQuienesomos}
-              className="d-block mx-lg-auto img-fluid rounded-3 shadow-lg"
-              alt="quienes somos"
-              width="700"
-              height="500"
-              loading="lazy"
-            />
-          </div>
           <div className="col-lg-6">
             <h1 data-aos="fade-up" className="display-5 fw-bold text-body-emphasis lh-1 mb-3">
               Quiénes somos
@@ -34,6 +24,16 @@ useEffect(()=>{
               {/* <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button> */}
               {/* <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button> */}
             </div>
+          </div>
+          <div data-aos="fade-up" className="col-10 col-sm-8 col-lg-6">
+            <img 
+              src={ImgQuienesomos}
+              className="d-block mx-lg-auto img-fluid rounded-3 shadow-lg"
+              alt="quienes somos"
+              width="700"
+              height="500"
+              loading="lazy"
+            />
           </div>
         </div>
       </div>
